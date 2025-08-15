33f3d958d38a88cf37267897d6cee88e7090f032
trsthales <thales87@gmail.com>
2025-08-15 12:33:09 -0300

    Delete curso-springboot-udemy/target directory

--- curso-springboot-udemy/target/classes/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO springboot.person (address,first_name,gender,last_name) VALUES
-	 ('Cariacica','Thales','Male','Ramalho'),
-	 ('Cariacica','Murillo','Male','Ramalho'),
-	 ('Vitoria','Rodolfo','Male','Rocha'),
-	 ('Mantena','Willian','Male','Daniel'),
-	 ('Vitoria','Simone','Female','Ramalho'),
-	 ('EUA','Martin','Male','Scorssese');
